237db70fb5ed96566e8579487a4491a2e5722750
Yang Dong <yangdong625@outlook.com>
2019-06-30 15:16:41 +0800

    Update service.ts

--- src/pages/user/new-competition/service.ts
+++ src/pages/user/new-competition/service.ts
@@ -2,7 +2,7 @@ import request from 'umi-request';
 import { NewCompetitionParams } from './index';
 
 export async function NewCompetitionSubmit(params: NewCompetitionParams) {
-  return request('http://liuterry.cn:8080/studentReg/', {
+  return request('http://liuterry.cn:8080/addCompetition/', {
     method: 'POST',
     data: params,
   });
