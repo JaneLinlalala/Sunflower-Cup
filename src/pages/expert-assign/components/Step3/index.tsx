86c6ed84bc88b2e1aafd19571c9d99270fe44c04
Yang Dong <yangdong625@outlook.com>
2019-07-01 14:36:21 +0800

    更新了分配专家的完成页面

--- src/pages/expert-assign/components/Step3/index.tsx
+++ src/pages/expert-assign/components/Step3/index.tsx
@@ -1,83 +1,42 @@
-import { Button, Col, Row } from 'antd';
-import React, { Fragment } from 'react';
+import { Button } from 'antd';
 import { Dispatch } from 'redux';
+// eslint-disable-next-line sort-imports
+import React, { Fragment } from 'react';
 import { connect } from 'dva';
+// eslint-disable-next-line sort-imports
 import { StateType } from '../../model';
+// eslint-disable-next-line sort-imports
 import Result from '../Result';
 import styles from './index.less';
 
 interface Step3Props {
   data?: StateType['step'];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   dispatch?: Dispatch<any>;
 }
 
 const Step3: React.FC<Step3Props> = props => {
-  const { data, dispatch } = props;
+  const { data } = props;
   if (!data) {
     return null;
   }
   const onFinish = () => {
-    if (dispatch) {
-      dispatch({
-        type: 'formStepForm/saveCurrentStep',
-        payload: 'info',
-      });
-    }
+    // TODO 返回到团委的首页
+    // if (dispatch) {
+    //   dispatch({
+    //     type: 'formStepForm/saveCurrentStep',
+    //     payload: 'info',
+    //   });
+    // }
   };
-  const information = (
-    <div className={styles.information}>
-      <Row>
-        <Col xs={24} sm={8} className={styles.label}>
-          付款账户：
-        </Col>
-        <Col xs={24} sm={16}>
-          {data.payAccount}
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={24} sm={8} className={styles.label}>
-          收款账户：
-        </Col>
-        <Col xs={24} sm={16}>
-          {data.receiverAccount}
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={24} sm={8} className={styles.label}>
-          收款人姓名：
-        </Col>
-        <Col xs={24} sm={16}>
-          {data.receiverName}
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={24} sm={8} className={styles.label}>
-          转账金额：
-        </Col>
-        <Col xs={24} sm={16}>
-          <span className={styles.money}>{data.amount}</span> 元
-        </Col>
-      </Row>
-    </div>
-  );
   const actions = (
     <Fragment>
       <Button type="primary" onClick={onFinish}>
-        再转一笔
+        返回
       </Button>
-      <Button>查看账单</Button>
     </Fragment>
   );
-  return (
-    <Result
-      type="success"
-      title="操作成功"
-      description="预计两小时内到账"
-      extra={information}
-      actions={actions}
-      className={styles.result}
-    />
-  );
+  return <Result type="success" title="操作成功" actions={actions} className={styles.result} />;
 };
 
 export default connect(({ formStepForm }: { formStepForm: StateType }) => ({
