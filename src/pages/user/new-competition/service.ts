5af707bc8aee35f0627e4a5553033b63c7715aac
Yang Dong <yangdong625@outlook.com>
2019-07-01 15:07:35 +0800

    删除了user下的页面

--- src/pages/user/new-competition/service.ts
+++
@@ -1,9 +0,0 @@
-import request from 'umi-request';
-import { NewCompetitionParams } from './index';
-
-export async function NewCompetitionSubmit(params: NewCompetitionParams) {
-  return request('http://liuterry.cn:8080/addCompetition', {
-    method: 'POST',
-    data: params,
-  });
-}
