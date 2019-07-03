8d77b8995f5dd1c0169e71b72dc23f8415563d6e
WInjace <15811599533@163.com>
2019-07-03 17:14:12 +0800

    finish 首页

--- src/pages/user/workplace/_mock.ts
+++ src/pages/user/workplace/_mock.ts
@@ -19,19 +19,6 @@ const avatars = [
   'https://gw.alipayobjects.com/zos/rmsportal/nxkuOJlFJuAUhzlMTCEe.png', // Webpack
 ];
 
-const avatars2 = [
-  'https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png',
-  'https://gw.alipayobjects.com/zos/rmsportal/cnrhVkzwxjPwAaCfPbdc.png',
-  'https://gw.alipayobjects.com/zos/rmsportal/gaOngJwsRYRaVAuXXcmB.png',
-  'https://gw.alipayobjects.com/zos/rmsportal/ubnKSIfAJTxIgXOKlciN.png',
-  'https://gw.alipayobjects.com/zos/rmsportal/WhxKECPNujWoWEFNdnJE.png',
-  'https://gw.alipayobjects.com/zos/rmsportal/jZUIxmJycoymBprLOUbT.png',
-  'https://gw.alipayobjects.com/zos/rmsportal/psOgztMplJMGpVEqfcgF.png',
-  'https://gw.alipayobjects.com/zos/rmsportal/ZpBqSxLxVEXfcUNoPKrz.png',
-  'https://gw.alipayobjects.com/zos/rmsportal/laiEnJdGHVOhJrUShBaJ.png',
-  'https://gw.alipayobjects.com/zos/rmsportal/UrQsqscbKEpNuJcvBZBu.png',
-];
-
 const getNotice = [
   {
     id: 'xxx1',
@@ -99,100 +86,38 @@ const getActivities = [
   {
     id: 'trend-1',
     updatedAt: new Date(),
-    user: {
-      name: '曲丽丽',
-      avatar: avatars2[0],
+    competition: {
+      name: '北京航空航天大学第29届冯如杯科技创新作品竞赛',
+      link: '/user/login',
     },
-    group: {
-      name: '高逼格设计天团',
-      link: 'http://github.com/',
-    },
-    project: {
-      name: '六月迭代',
-      link: 'http://github.com/',
-    },
-    template: '在 @{group} 新建项目 @{project}',
+    template: '@{competition}',
   },
   {
-    id: 'trend-2',
+    id: 'trend-1',
     updatedAt: new Date(),
-    user: {
-      name: '付小小',
-      avatar: avatars2[1],
+    competition: {
+      name: '北京航空航天大学第30届冯如杯科技创新作品竞赛',
+      link: 'http://www.baidu.com',
     },
-    group: {
-      name: '高逼格设计天团',
-      link: 'http://github.com/',
-    },
-    project: {
-      name: '六月迭代',
-      link: 'http://github.com/',
-    },
-    template: '在 @{group} 新建项目 @{project}',
+    template: '@{competition}',
   },
   {
-    id: 'trend-3',
+    id: 'trend-1',
     updatedAt: new Date(),
-    user: {
-      name: '林东东',
-      avatar: avatars2[2],
+    competition: {
+      name: '北京航空航天大学第31届冯如杯科技创新作品竞赛',
+      link: 'http://www.baidu.com',
     },
-    group: {
-      name: '中二少女团',
-      link: 'http://github.com/',
-    },
-    project: {
-      name: '六月迭代',
-      link: 'http://github.com/',
-    },
-    template: '在 @{group} 新建项目 @{project}',
+    template: '@{competition}',
   },
   {
-    id: 'trend-4',
+    id: 'trend-1',
     updatedAt: new Date(),
-    user: {
-      name: '周星星',
-      avatar: avatars2[4],
+    competition: {
+      name: '北京航空航天大学第32届冯如杯科技创新作品竞赛',
+      link: 'http://www.baidu.com',
     },
-    project: {
-      name: '5 月日常迭代',
-      link: 'http://github.com/',
-    },
-    template: '将 @{project} 更新至已发布状态',
-  },
-  {
-    id: 'trend-5',
-    updatedAt: new Date(),
-    user: {
-      name: '朱偏右',
-      avatar: avatars2[3],
-    },
-    project: {
-      name: '工程效能',
-      link: 'http://github.com/',
-    },
-    comment: {
-      name: '留言',
-      link: 'http://github.com/',
-    },
-    template: '在 @{project} 发布了 @{comment}',
-  },
-  {
-    id: 'trend-6',
-    updatedAt: new Date(),
-    user: {
-      name: '乐哥',
-      avatar: avatars2[5],
-    },
-    group: {
-      name: '程序员日常',
-      link: 'http://github.com/',
-    },
-    project: {
-      name: '品牌迭代',
-      link: 'http://github.com/',
-    },
-    template: '在 @{group} 新建项目 @{project}',
+    template: '@{competition}',
   },
 ];
 
--- src/pages/user/workplace/index.tsx
+++ src/pages/user/workplace/index.tsx
@@ -2,44 +2,14 @@ import { Avatar, Card, Col, List, Skeleton, Row, Statistic } from 'antd';
 import React, { Component } from 'react';
 
 import { Dispatch } from 'redux';
-import Link from 'umi/link';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { connect } from 'dva';
 import moment from 'moment';
-import Radar from './components/Radar';
 import { ModalState } from './model';
-import EditableLinkGroup from './components/EditableLinkGroup';
 import styles from './style.less';
 import { ActivitiesType, CurrentUser, NoticeType, RadarDataType } from './data.d';
 
-const links = [
-  {
-    title: '操作一',
-    href: '',
-  },
-  {
-    title: '操作二',
-    href: '',
-  },
-  {
-    title: '操作三',
-    href: '',
-  },
-  {
-    title: '操作四',
-    href: '',
-  },
-  {
-    title: '操作五',
-    href: '',
-  },
-  {
-    title: '操作六',
-    href: '',
-  },
-];
-
-interface dashboardWorkplaceProps {
+interface DashboardWorkplaceProps {
   currentUser: CurrentUser;
   projectNotice: NoticeType[];
   activities: ActivitiesType[];
@@ -74,20 +44,6 @@ const PageHeaderContent: React.FC<{ currentUser: CurrentUser }> = ({ currentUser
   );
 };
 
-const ExtraContent: React.FC<{}> = () => (
-  <div className={styles.extraContent}>
-    <div className={styles.statItem}>
-      <Statistic title="项目数" value={56} />
-    </div>
-    <div className={styles.statItem}>
-      <Statistic title="团队内排名" value={8} suffix="/ 24" />
-    </div>
-    <div className={styles.statItem}>
-      <Statistic title="项目访问" value={2223} />
-    </div>
-  </div>
-);
-
 @connect(
   ({
     dashboardWorkplace: { currentUser, projectNotice, activities, radarData },
@@ -105,7 +61,7 @@ const ExtraContent: React.FC<{}> = () => (
     activitiesLoading: loading.effects['dashboardWorkplace/fetchActivitiesList'],
   }),
 )
-class Workplace extends Component<dashboardWorkplaceProps> {
+class Workplace extends Component<DashboardWorkplaceProps> {
   componentDidMount() {
     const { dispatch } = this.props;
     dispatch({
@@ -134,11 +90,8 @@ class Workplace extends Component<dashboardWorkplaceProps> {
     return (
       <List.Item key={item.id}>
         <List.Item.Meta
-          avatar={<Avatar src={item.user.avatar} />}
           title={
             <span>
-              <a className={styles.username}>{item.user.name}</a>
-              &nbsp;
               <span className={styles.event}>{events}</span>
             </span>
           }
@@ -153,111 +106,25 @@ class Workplace extends Component<dashboardWorkplaceProps> {
   };
 
   render() {
-    const {
-      currentUser,
-      activities,
-      projectNotice,
-      projectLoading,
-      activitiesLoading,
-      radarData,
-    } = this.props;
+    const { currentUser, activities, activitiesLoading } = this.props;
 
     return (
-      <PageHeaderWrapper
-        content={<PageHeaderContent currentUser={currentUser} />}
-        extraContent={<ExtraContent />}
-      >
-        <Row gutter={24}>
-          <Col xl={16} lg={24} md={24} sm={24} xs={24}>
-            <Card
-              className={styles.projectList}
-              style={{ marginBottom: 24 }}
-              title="进行中的项目"
-              bordered={false}
-              extra={<Link to="/">全部项目</Link>}
-              loading={projectLoading}
-              bodyStyle={{ padding: 0 }}
-            >
-              {projectNotice.map(item => (
-                <Card.Grid className={styles.projectGrid} key={item.id}>
-                  <Card bodyStyle={{ padding: 0 }} bordered={false}>
-                    <Card.Meta
-                      title={
-                        <div className={styles.cardTitle}>
-                          <Avatar size="small" src={item.logo} />
-                          <Link to={item.href}>{item.title}</Link>
-                        </div>
-                      }
-                      description={item.description}
-                    />
-                    <div className={styles.projectItemContent}>
-                      <Link to={item.memberLink}>{item.member || ''}</Link>
-                      {item.updatedAt && (
-                        <span className={styles.datetime} title={item.updatedAt}>
-                          {moment(item.updatedAt).fromNow()}
-                        </span>
-                      )}
-                    </div>
-                  </Card>
-                </Card.Grid>
-              ))}
-            </Card>
-            <Card
-              bodyStyle={{ padding: 0 }}
-              bordered={false}
-              className={styles.activeCard}
-              title="动态"
-              loading={activitiesLoading}
-            >
-              <List<ActivitiesType>
-                loading={activitiesLoading}
-                renderItem={item => this.renderActivities(item)}
-                dataSource={activities}
-                className={styles.activitiesList}
-                size="large"
-              />
-            </Card>
-          </Col>
-          <Col xl={8} lg={24} md={24} sm={24} xs={24}>
-            <Card
-              style={{ marginBottom: 24 }}
-              title="快速开始 / 便捷导航"
-              bordered={false}
-              bodyStyle={{ padding: 0 }}
-            >
-              <EditableLinkGroup onAdd={() => {}} links={links} linkElement={Link} />
-            </Card>
-            <Card
-              style={{ marginBottom: 24 }}
-              bordered={false}
-              title="XX 指数"
-              loading={radarData.length === 0}
-            >
-              <div className={styles.chart}>
-                <Radar hasLegend height={343} data={radarData} />
-              </div>
-            </Card>
-            <Card
-              bodyStyle={{ paddingTop: 12, paddingBottom: 12 }}
-              bordered={false}
-              title="团队"
-              loading={projectLoading}
-            >
-              <div className={styles.members}>
-                <Row gutter={48}>
-                  {projectNotice.map(item => (
-                    <Col span={12} key={`members-item-${item.id}`}>
-                      <Link to={item.href}>
-                        <Avatar src={item.logo} size="small" />
-                        <span className={styles.member}>{item.member}</span>
-                      </Link>
-                    </Col>
-                  ))}
-                </Row>
-              </div>
-            </Card>
-          </Col>
-        </Row>
+      <PageHeaderWrapper content={<PageHeaderContent currentUser={currentUser} />}>
+        <Card
+          bodyStyle={{ padding: 0 }}
+          bordered={false}
+          className={styles.activeCard}
+          title="动态"
+          loading={activitiesLoading}
+        >
+          <List<ActivitiesType>
+            loading={activitiesLoading}
+            renderItem={item => this.renderActivities(item)}
+            dataSource={activities}
+            className={styles.activitiesList}
+            size="large"
+          />
+        </Card>
       </PageHeaderWrapper>
     );
   }
