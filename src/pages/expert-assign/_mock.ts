f7865825b2dee4d672dd7cb22d1e8fc1339bb297
Yang Dong <yangdong625@outlook.com>
2019-07-03 22:21:43 +0800

    删除了expert-assign

--- src/pages/expert-assign/_mock.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  'POST  /api/forms': (req: any, res: { send: (arg0: { message: string }) => void }) => {
-    res.send({ message: 'Ok' });
-  },
-};
